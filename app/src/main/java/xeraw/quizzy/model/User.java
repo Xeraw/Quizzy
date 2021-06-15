650410ea85b475ba5e81b0c71722fde81014d232
Xeraw <Tony_ledoux@hotmail.fr>
2021-06-15 13:21:07 +0200

    first commit (app)

---
+++ app/src/main/java/xeraw/quizzy/model/User.java
@@ -0,0 +1,24 @@
+package xeraw.quizzy.model;
+
+/**
+ * Created by Xeraw on 25/02/2021
+ */
+public class User {
+
+    private String mFirstname;
+
+    public String getFirstname() {
+        return mFirstname;
+    }
+
+    public void setFirstname(String firstname) {
+        mFirstname = firstname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mFirstname='" + mFirstname + '\'' +
+                '}';
+    }
+}
